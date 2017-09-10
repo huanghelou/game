7a18b20bf946f0a8e50304d835de389efb1ea60e
YangMaoMao <15828351114@163.com>
2017-09-10 14:09:32 +0800

    many file commit

---
+++ 俄罗斯方块/tetris.js
@@ -0,0 +1,9 @@
+/**
+ * Created by YangZe on 2017/9/9.
+ */
+
+var NUM_ROWS = 20;          //游戏区域横着的格子数
+var NUM_COLS = 10;          //游戏区域竖着的格子数
+var BLOCK_WIDTH = 30;       //方块的宽度
+var BLOCK_HEIGHT = 30;      //方块的长度
+var TICK_MS = 30;
